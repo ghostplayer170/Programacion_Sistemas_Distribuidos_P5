36a3d204337bd093ebfda4935cf76315ed66afd6
ghostplayer170 <82672414+ghostplayer170@users.noreply.github.com>
2024-05-17 12:34:45 +0200

    final-app

--- node/node.py
+++ node/node.py
@@ -5,42 +5,53 @@ import time
 import sys
 from threading import Thread
 
-app = Flask(__name__)
+app = Flask(__name__) # Create a Flask app
 node_id = int(os.getenv('NODE_ID', '80'))  # Defaulting to 80 if NODE_ID is not set
-registry_url = "http://registry:8080"
-nodes = []
+registry_url = os.getenv('REGISTRY_URL', 'http://registry:8080') # Defaulting to http://registry:8080 if REGISTRY_URL is not set
+nodes = [] # List of nodes
 current_coordinator = None
 election_in_progress = False
 
 
 def log_message(message):
     print(message)
-    sys.stdout.flush()
+    sys.stdout.flush() # Flush the stdout buffer to ensure logs are written immediately
 
 
 def send_heartbeat():
     while True:
         try:
+            # Send a heartbeat to the registry
             requests.post(f"{registry_url}/heartbeat", json={'node': node_id})
             log_message(
                 f"Sent heartbeat from node {node_id}, current coordinator is {current_coordinator} among {nodes}")
-            time.sleep(30)
+            time.sleep(30) # Send heartbeat every 30 seconds
+            check_current_coordinator() # Check the current coordinator
         except requests.RequestException as e:
             log_message(f"Error sending heartbeat from node {node_id}: {e}")
+
+def check_current_coordinator(): # if current coordinator id is less than the node id, then start the election
+    global node_id, current_coordinator, election_in_progress
+    if current_coordinator is not None and current_coordinator < node_id:
+        log_message(f"Node {node_id} detected that it has a higher ID than the current coordinator {current_coordinator}. Initiating election.")
+        initiate_election()
 
 
 def register_with_registry():
     node_address = f"node{node_id}:80"
     try:
-        # Register the node and receive the list of current nodes
+        # Register the node with the registry
         response = requests.post(f"{registry_url}/register", json={'node': node_id, 'address': node_address})
-        global nodes
-        nodes = response.json()
-        log_message(f"Registered with nodes: {nodes}")
-        # After registering, ask for the current coordinator
-        update_peers(nodes)
+        if response.status_code == 200:
+            global nodes
+            nodes = response.json()
+            log_message(f"Node {node_id} registered successfully with registry. Active nodes: {nodes}")
+            update_peers(nodes)
+        else:
+            log_message(f"Failed to register node {node_id} with registry.")
     except requests.RequestException as e:
-        log_message(f"Error registering node {node_id}: {e}")
+        log_message(f"Error registering node {node_id} with registry: {e}")
+
 
 
 def update_peers(new_peers):
@@ -53,7 +64,7 @@ def update_peers(new_peers):
         # Attempt to fetch the current coordinator from peers
         if not election_in_progress:
             log_message(f"Node {node_id} fetching coordinator from peers.")
-            fetch_coordinator_from_peers()
+            fetch_coordinator_from_peers() # Fetch the coordinator from the peers to update the current coordinator
 
         # Election initiation logic
         if current_coordinator not in nodes:  # If current coordinator is no longer available
@@ -92,13 +103,7 @@ def initiate_election():
         return
     election_in_progress = True
     log_message(f"Node {node_id} initiating election among {nodes}")
-    pass_election_message(node_id, node_id)
-
-
-@app.route('/coordinator_info', methods=['GET'])
-def get_coordinator_info():
-    global current_coordinator
-    return {'coordinator': current_coordinator}, 200
+    pass_election_message(node_id, node_id) # Pass the election message
 
 
 def pass_election_message(initiator, max_id):
@@ -106,13 +111,14 @@ def pass_election_message(initiator, max_id):
 
     nodes.sort()
 
-    if len(nodes) <= 1:
+    if len(nodes) <= 1: # If there is only one node in the network
         log_message(f"Node {node_id} is the only node in the network. Elected as coordinator.")
         current_coordinator = node_id
-        notify_all_coordinator()
         election_in_progress = False
         return
 
+    # Get the next node to pass the election message
+    # The next node is the one after the current node in the sorted list
     current_index = nodes.index(node_id)
     next_node_index = (current_index + 1) % len(nodes)
     next_node = nodes[next_node_index]
@@ -120,15 +126,18 @@ def pass_election_message(initiator, max_id):
     log_message(
         f"Passing election message from node {node_id} to node {next_node} with initiator {initiator} and max_id {max_id}")
 
+    # If the next node is the initiator, the election is complete
     if next_node == initiator:
-        if max_id == node_id:
+        if max_id == node_id: # If the current node has the highest ID
             current_coordinator = node_id
-        else:
+        else: # If the current node does not have the highest ID
             current_coordinator = max_id
         notify_all_coordinator()
         election_in_progress = False
-    else:
+    else: # Pass the election message to the next node
         try:
+            # Pass the election message to the next node
+            # Max ID is the maximum of the current node's ID and the max ID seen so far in the network
             response = requests.post(f'http://node{next_node}:80/election',
                                      json={'initiator': initiator, 'max_id': max(max_id, node_id)})
             if response.status_code != 200:
@@ -142,6 +151,7 @@ def notify_all_coordinator():
     for peer in nodes:
         if peer != node_id:  # No need to notify self
             try:
+                # Notify the peer about the new coordinator in the network to update their records
                 response = requests.post(f'http://node{peer}:80/coordinator', json={'coordinator': current_coordinator})
                 if response.status_code == 200:
                     log_message(f"Node {peer} acknowledged {current_coordinator} as coordinator.")
@@ -150,7 +160,13 @@ def notify_all_coordinator():
     log_message(f"Election completed. Node {current_coordinator} is the coordinator among {nodes}.")
 
 
-@app.route('/check_nodes', methods=['POST'])
+@app.route('/coordinator_info', methods=['GET']) # Get the coordinator info
+def get_coordinator_info():
+    global current_coordinator
+    return {'coordinator': current_coordinator}, 200
+
+
+@app.route('/check_nodes', methods=['POST']) # Check the nodes and update the peers
 def check_nodes():
     try:
         response = requests.get(f"{registry_url}/nodes")
@@ -161,29 +177,30 @@ def check_nodes():
     return 'Checked nodes and updated peers', 200
 
 
-@app.route('/election', methods=['POST'])
+@app.route('/election', methods=['POST']) # Pass the election message
 def election():
+    # Get the initiator and max_id from the request
     data = request.json
     initiator = data['initiator']
     max_id = data['max_id']
-    pass_election_message(initiator, max_id)
+    pass_election_message(initiator, max_id) # Pass the election message
     return 'Election message passed on', 200
 
 
-@app.route('/coordinator', methods=['POST'])
+@app.route('/coordinator', methods=['POST']) # Update the coordinator
 def receive_coordinator_notification():
     global current_coordinator, election_in_progress
     incoming_coordinator = request.json['coordinator']
-    if current_coordinator != incoming_coordinator:
+    if current_coordinator != incoming_coordinator: # Update the coordinator if it has changed
         current_coordinator = incoming_coordinator
         log_message(f"Node {node_id} updated coordinator to {current_coordinator}.")
-        notify_all_coordinator()
-    election_in_progress = False
+        notify_all_coordinator() # Notify all nodes about the new coordinator
+    election_in_progress = False # Reset the election flag
     return 'Coordinator updated', 200
 
 
 if __name__ == '__main__':
-    register_with_registry()
-    peer_refresh_thread = Thread(target=send_heartbeat)
-    peer_refresh_thread.start()
+    register_with_registry() # Register with the registry
+    peer_refresh_thread = Thread(target=send_heartbeat) # Create a thread for the heartbeat
+    peer_refresh_thread.start() # Start the heartbeat thread
     app.run(host='0.0.0.0', port=80)  # Each node listens on its NODE_ID as port
--- registry/registry.py
+++ registry/registry.py
@@ -11,36 +11,38 @@ nodes = {}  # Node ID mapped to {last_seen: datetime, address: str}
 
 def log_message(message):
     print(message)
-    sys.stdout.flush()
+    sys.stdout.flush() # Flush the stdout buffer to ensure logs are written immediately
 
 
 def check_inactive_nodes():
-    global initial_nodes_handled
+    global initial_nodes_handled # Flag to check if initial nodes have been handled
     if not initial_nodes_handled:
         initial_nodes()
         initial_nodes_handled = True
     while True:
-        time.sleep(30)  # Periodic check
+        time.sleep(30)  # Time interval to check for inactive nodes
         current_time = datetime.now()
-        to_remove = []
+        to_remove = [] # List of nodes to remove
         with threading.Lock():
             if not nodes:
                 log_message("No nodes registered yet.")
                 continue
             for node, info in list(nodes.items()):
+                # Check if the node is inactive for more than 30 seconds
                 if current_time - info['last_seen'] > timedelta(seconds=30):
                     to_remove.append(node)
-            for node in to_remove:
+            for node in to_remove: # Remove the inactive node
                 del nodes[node]
                 log_message(f"Detected inactive node: {node}. Triggering notification to active nodes.")
-            if to_remove:
+            if to_remove: # Notify active nodes about the change
                 notify_nodes_of_change()
 
 
-def notify_nodes_of_change():
-    items = list(nodes.items())
+def notify_nodes_of_change(): # Notify all nodes about the change
+    items = list(nodes.items()) # Get a snapshot of the nodes
     for node, info in items:
         try:
+            # Notify the node about the change
             requests.post(f"http://{info['address']}/check_nodes", json={'nodes': list(nodes.keys())})
             log_message(f"Node {node} at {info['address']} has been notified about the change.")
         except Exception as e:
@@ -48,7 +50,7 @@ def notify_nodes_of_change():
 
 
 def initial_nodes():
-    time.sleep(15)  # Wait for 25 seconds after startup
+    time.sleep(15)  # Wait for 15 seconds after startup
     with threading.Lock():
         if nodes:
             log_message(f"Initial nodes: {list(nodes.keys())}")
@@ -57,41 +59,45 @@ def initial_nodes():
             log_message("No nodes registered yet.")
 
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['POST']) # Register a new node
 def register():
+    # Get the node ID and address from the request
     node_data = request.json
     node_id = node_data['node']
     node_address = node_data['address']
 
-    with threading.Lock():
-        new_node = node_id not in nodes
-        log_message(f"New node: {new_node}")
+    # Register the node
+    with threading.Lock(): # Lock the thread
+        new_node = node_id not in nodes # Check if the node is new
         nodes[node_id] = {'last_seen': datetime.now(), 'address': node_address}
-        log_message(f"Node {node_id} registered or updated with address {node_address}.")
+        log_message(f"Node {node_id} registered successfully and updated the last seen time.")
 
     # if new node are registered, notify all nodes about the change
     if new_node and initial_nodes_handled:
         log_message(f"Triggering notification to all nodes about the new node {node_id}.")
         notify_nodes_of_change()
 
+    # Return the list of all nodes
     return jsonify(list(nodes.keys())), 200
 
 
-@app.route('/heartbeat', methods=['POST'])
+@app.route('/heartbeat', methods=['POST']) # Heartbeat
 def heartbeat():
-    node = request.json.get('node')
+    node = request.json.get('node') # Get the node ID from the request
     with threading.Lock():
         if node in nodes:
             nodes[node]['last_seen'] = datetime.now()  # Update the heartbeat timestamp
     return 'OK', 200
 
 
-@app.route('/nodes', methods=['GET'])
+@app.route('/nodes', methods=['GET']) # Get all nodes
 def get_nodes():
     return jsonify(list(nodes.keys())), 200
 
 
 if __name__ == '__main__':
     initial_nodes_handled = False
+    # Start the thread to check inactive nodes
     threading.Thread(target=check_inactive_nodes, daemon=True).start()
+    # Start the Flask app
     app.run(host='0.0.0.0', port=8080, threaded=True)
